87bf9d62da79cff85f624d98790945c5fb6bfdac
Kyle Cornwall <kyle.cornwall@healthcatalyst.com>
2018-08-16 14:09:44 -0600

    - Modified .gitignore

---
+++ client-dev/src/app/services/auth/auth0-variables.ts
@@ -0,0 +1,7 @@
+import { AuthConfig } from '../../models/auth.config';
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: 'JL58LF6ccu98nTToPa5vUsm0aVObRjef',
+  domain: 'ckylegaming.auth0.com',
+  callbackURL: 'http://localhost:4200/callback'
+};
