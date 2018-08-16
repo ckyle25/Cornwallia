d3e804793a253b8f679be5faf82b5bc0c102c05a
Kyle Cornwall <kyle.cornwall@healthcatalyst.com>
2018-08-16 16:39:38 -0600

    - undo test.

--- server/controllers/configController.js
+++ server/controllers/configController.js
@@ -1,14 +1,9 @@
 module.exports = {
     getConfig: (req, res, next) => {
-        // res.status(200).send({
-        //     clientID: process.env.AUTH_CLIENT_ID,
-        //     callbackUrl: process.env.AUTH_CALLBACK_URL,
-        //     domain: process.env.AUTH_DOMAIN
-        // })
         res.status(200).send({
-            clientID: 'test client',
-            callbackUrl: 'test callback',
-            domain: 'test domain'
+            clientID: process.env.AUTH_CLIENT_ID,
+            callbackUrl: process.env.AUTH_CALLBACK_URL,
+            domain: process.env.AUTH_DOMAIN
         })
     }
 }
