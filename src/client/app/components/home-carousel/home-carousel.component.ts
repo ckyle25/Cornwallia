c09a21033f9016b690ec3783d65584f531a53763
Kyle Cornwall <kyle.cornwall@healthcatalyst.com>
2018-09-04 16:28:47 -0600

    - Changed styling a bit.

--- src/client/app/components/home-carousel/home-carousel.component.ts
+++ src/client/app/components/home-carousel/home-carousel.component.ts
@@ -10,6 +10,26 @@ export class HomeCarouselComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.selectF();
   }
 
+  selectF() {
+    document.getElementById('F').click();
+    setTimeout(() => this.selectG(), 5000);
+  }
+
+  selectG() {
+    document.getElementById('G').click();
+    setTimeout(() => this.selectH(), 5000);
+  }
+
+  selectH() {
+    document.getElementById('H').click();
+    setTimeout(() => this.selectI(), 5000);
+  }
+
+  selectI() {
+    document.getElementById('I').click();
+    setTimeout(() => this.selectF(), 5000);
+  }
 }
