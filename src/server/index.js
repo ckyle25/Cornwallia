93b141d498ce4cbaa067dbd77ce1220bc9be63a9
Kyle Cornwall <kyle.cornwall@healthcatalyst.com>
2018-12-21 13:08:09 -0700

    - Turning off email

--- src/server/index.js
+++ src/server/index.js
@@ -195,165 +195,165 @@ app.post(`${baseUrl}/food/getActiveUser`, checkAuthenticated, foodGetActiveUser)
 
 // Wishes Email Services
 // let j = schedule.scheduleJob('1 * * * * *', () => {
-let j = schedule.scheduleJob('20 * * *', () => {
-  app.get('db').check_birthdays()
-  .then(result => {
-      const today = moment(new Date());
-      let birthdayUpdateList = [];
+// let j = schedule.scheduleJob('20 * * *', () => {
+//   app.get('db').check_birthdays()
+//   .then(result => {
+//       const today = moment(new Date());
+//       let birthdayUpdateList = [];
 
-      result.forEach((user) => {
-        var birthDay = moment(user.birthdaydt).year(today.year());
-        var birthDayNextYear = moment(user.birthdaydt).year(today.year() + 1);
-        var daysRemainingThisYear = birthDay.diff(today, 'days');
-        var daysRemainingNextYear = birthDayNextYear.diff(today, 'days');
+//       result.forEach((user) => {
+//         var birthDay = moment(user.birthdaydt).year(today.year());
+//         var birthDayNextYear = moment(user.birthdaydt).year(today.year() + 1);
+//         var daysRemainingThisYear = birthDay.diff(today, 'days');
+//         var daysRemainingNextYear = birthDayNextYear.diff(today, 'days');
 
-        if (daysRemainingThisYear == 30 || daysRemainingNextYear == 30) {
-          birthdayUpdateList.push(user);
-        }
+//         if (daysRemainingThisYear == 30 || daysRemainingNextYear == 30) {
+//           birthdayUpdateList.push(user);
+//         }
 
-        if (daysRemainingThisYear == 14 || daysRemainingNextYear == 14) {
+//         if (daysRemainingThisYear == 14 || daysRemainingNextYear == 14) {
 
-          let familyList = '';
+//           let familyList = '';
 
-          switch (user.familyid) {
-            case 1:
-              familyList = '{1,2,3,4,5,6,7}';
-              break;
-            case 2:
-              familyList = '{1,2,3,4}';
-              break;
-            case 3:
-              familyList = '{1,2,3,4}';
-              break;
-            case 4:
-              familyList = '{1,2,3,4}';
-              break;
-            case 5:
-              familyList = '{1,5,6}';
-              break;
-            case 6:
-              familyList = '{1,5,6,7}';
-              break;
-            case 7:
-              familyList = '{1,6,7}';
-              break;
-          }
+//           switch (user.familyid) {
+//             case 1:
+//               familyList = '{1,2,3,4,5,6,7}';
+//               break;
+//             case 2:
+//               familyList = '{1,2,3,4}';
+//               break;
+//             case 3:
+//               familyList = '{1,2,3,4}';
+//               break;
+//             case 4:
+//               familyList = '{1,2,3,4}';
+//               break;
+//             case 5:
+//               familyList = '{1,5,6}';
+//               break;
+//             case 6:
+//               familyList = '{1,5,6,7}';
+//               break;
+//             case 7:
+//               familyList = '{1,6,7}';
+//               break;
+//           }
 
-        app.get('db').get_birthday_checkout([user.userid, familyList])
-          .then(result => {
-            result.forEach(user => {
-              const mailOptions = {
-                from: 'Cornwallia Wishes <cornwallia225@gmail.com>',
-                to: user.emaildsc,
-                subject: `${user.birthdayperson}'s Birthday Reminder: Login to View Wishes`,
-                text: `Hi ${user.firstnameval},\n\nIt looks like ${user.birthdayperson}'s birthday is coming on ${moment(user.birthdaydt).format("MMM DD")}. Login to the wishes app to view their updated wishes.\nLink: https://cornwallia.azurewebsites.net/\n\nRegards,\n\nCornwallia Wishes Admin`
-              };
+//         app.get('db').get_birthday_checkout([user.userid, familyList])
+//           .then(result => {
+//             result.forEach(user => {
+//               const mailOptions = {
+//                 from: 'Cornwallia Wishes <cornwallia225@gmail.com>',
+//                 to: user.emaildsc,
+//                 subject: `${user.birthdayperson}'s Birthday Reminder: Login to View Wishes`,
+//                 text: `Hi ${user.firstnameval},\n\nIt looks like ${user.birthdayperson}'s birthday is coming on ${moment(user.birthdaydt).format("MMM DD")}. Login to the wishes app to view their updated wishes.\nLink: https://cornwallia.azurewebsites.net/\n\nRegards,\n\nCornwallia Wishes Admin`
+//               };
 
-              transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                  console.log(error);
-                } else {
-                  return res.status(200).send('Email sent')
-                }
-              });
-            })
-          });
-        }
+//               transporter.sendMail(mailOptions, (error, info) => {
+//                 if (error) {
+//                   console.log(error);
+//                 } else {
+//                   return res.status(200).send('Email sent')
+//                 }
+//               });
+//             })
+//           });
+//         }
 
-      });
+//       });
 
-      // const testEmailArray = [
-      //   {
-      //     userid: 12,
-      //     firstnameval: 'Kyle',
-      //     birthdaydt: '1964-01-08T07:00:00.000Z',
-      //     edwuserid: null,
-      //     primaryemail: 'ckyle25@gmail.com',
-      //     parent1name: 'Kyle',
-      //     parent1edwuserid: null,
-      //     parent1email: 'ckyle25@gmail.com',
-      //     parent2name: null,
-      //     parent2edwuserid: null,
-      //     parent2email: null
-      //   },
-      //   {
-      //     userid: 7,
-      //     firstnameval: 'Lily',
-      //     birthdaydt: '2000-01-30T07:00:00.000Z',
-      //     edwuserid: null,
-      //     primaryemail: null,
-      //     familyid: null,
-      //     parent1name: 'Kevin',
-      //     parent1edwuserid: 9,
-      //     parent1email: 'fflm.cornwall@gmail.com',
-      //     parent2name: 'Kendal',
-      //     parent2edwuserid: 8,
-      //     parent2email: 'kendollnorman@gmail.com'
-      //   }
-      // ]
+//       // const testEmailArray = [
+//       //   {
+//       //     userid: 12,
+//       //     firstnameval: 'Kyle',
+//       //     birthdaydt: '1964-01-08T07:00:00.000Z',
+//       //     edwuserid: null,
+//       //     primaryemail: 'ckyle25@gmail.com',
+//       //     parent1name: 'Kyle',
+//       //     parent1edwuserid: null,
+//       //     parent1email: 'ckyle25@gmail.com',
+//       //     parent2name: null,
+//       //     parent2edwuserid: null,
+//       //     parent2email: null
+//       //   },
+//       //   {
+//       //     userid: 7,
+//       //     firstnameval: 'Lily',
+//       //     birthdaydt: '2000-01-30T07:00:00.000Z',
+//       //     edwuserid: null,
+//       //     primaryemail: null,
+//       //     familyid: null,
+//       //     parent1name: 'Kevin',
+//       //     parent1edwuserid: 9,
+//       //     parent1email: 'fflm.cornwall@gmail.com',
+//       //     parent2name: 'Kendal',
+//       //     parent2edwuserid: 8,
+//       //     parent2email: 'kendollnorman@gmail.com'
+//       //   }
+//       // ]
 
-      birthdayUpdateList.forEach(user => {
-        let toAddress = ''
+//       birthdayUpdateList.forEach(user => {
+//         let toAddress = ''
 
-        if (user.primaryemail) {
-          toAddress = user.primaryemail
+//         if (user.primaryemail) {
+//           toAddress = user.primaryemail
 
-          const mailOptions = {
-            from: 'Cornwallia Wishes <cornwallia225@gmail.com>',
-            to: toAddress,
-            subject: 'Birthday Reminder: Update your Wishes in Cornwallia',
-            text: `Hi ${user.firstnameval},\n\nIt looks like your birthday is coming up soon. When you get a second, login to the Wishes app to update your wish list!\nLink: https://cornwallia.azurewebsites.net/\n\nRegards,\n\nCornwallia Wishes Admin`
-          };
+//           const mailOptions = {
+//             from: 'Cornwallia Wishes <cornwallia225@gmail.com>',
+//             to: toAddress,
+//             subject: 'Birthday Reminder: Update your Wishes in Cornwallia',
+//             text: `Hi ${user.firstnameval},\n\nIt looks like your birthday is coming up soon. When you get a second, login to the Wishes app to update your wish list!\nLink: https://cornwallia.azurewebsites.net/\n\nRegards,\n\nCornwallia Wishes Admin`
+//           };
 
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              console.log(error);
-            } else {
-              return res.status(200).send('Email sent')
-            }
-          });
-        }
+//           transporter.sendMail(mailOptions, (error, info) => {
+//             if (error) {
+//               console.log(error);
+//             } else {
+//               return res.status(200).send('Email sent')
+//             }
+//           });
+//         }
 
-        if (!user.primaryemail && user.parent1email) {
-          toAddress = user.parent1email
+//         if (!user.primaryemail && user.parent1email) {
+//           toAddress = user.parent1email
 
-          const mailOptions = {
-            from: 'Cornwallia Wishes <cornwallia225@gmail.com>',
-            to: toAddress,
-            subject: `Birthday Reminder: Update ${user.firstnameval}'s Wishes in Cornwallia`,
-            text: `Hi ${user.parent1name},\n\nIt looks like ${user.firstnameval}'s birthday is coming up soon. When you get a second, login to the Wishes app to update their wish list!\nLink: https://cornwallia.azurewebsites.net/\n\nRegards,\n\nCornwallia Wishes Admin`
-          };
+//           const mailOptions = {
+//             from: 'Cornwallia Wishes <cornwallia225@gmail.com>',
+//             to: toAddress,
+//             subject: `Birthday Reminder: Update ${user.firstnameval}'s Wishes in Cornwallia`,
+//             text: `Hi ${user.parent1name},\n\nIt looks like ${user.firstnameval}'s birthday is coming up soon. When you get a second, login to the Wishes app to update their wish list!\nLink: https://cornwallia.azurewebsites.net/\n\nRegards,\n\nCornwallia Wishes Admin`
+//           };
 
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              console.log(error);
-            } else {
-              return res.status(200).send('Email sent')
-            }
-          });
-        }
+//           transporter.sendMail(mailOptions, (error, info) => {
+//             if (error) {
+//               console.log(error);
+//             } else {
+//               return res.status(200).send('Email sent')
+//             }
+//           });
+//         }
 
-        if (!user.primaryemail && user.parent2email) {
-          toAddress = user.parent2email
+//         if (!user.primaryemail && user.parent2email) {
+//           toAddress = user.parent2email
 
-          const mailOptions = {
-            from: 'Cornwallia Wishes <cornwallia225@gmail.com>',
-            to: toAddress,
-            subject: `Birthday Reminder: Update ${user.firstnameval}'s Wishes in Cornwallia`,
-            text: `Hi ${user.parent2name},\n\nIt looks like ${user.firstnameval}'s birthday is coming up soon. When you get a second, login to the Wishes app to update their wish list!\nLink: https://cornwallia.azurewebsites.net/\n\nRegards,\n\nCornwallia Wishes Admin`
-          };
+//           const mailOptions = {
+//             from: 'Cornwallia Wishes <cornwallia225@gmail.com>',
+//             to: toAddress,
+//             subject: `Birthday Reminder: Update ${user.firstnameval}'s Wishes in Cornwallia`,
+//             text: `Hi ${user.parent2name},\n\nIt looks like ${user.firstnameval}'s birthday is coming up soon. When you get a second, login to the Wishes app to update their wish list!\nLink: https://cornwallia.azurewebsites.net/\n\nRegards,\n\nCornwallia Wishes Admin`
+//           };
 
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              console.log(error);
-            } else {
-              return res.status(200).send('Email sent')
-            }
-          });
-        }
-      });
-  });
-})
+//           transporter.sendMail(mailOptions, (error, info) => {
+//             if (error) {
+//               console.log(error);
+//             } else {
+//               return res.status(200).send('Email sent')
+//             }
+//           });
+//         }
+//       });
+//   });
+// })
 
 const port = process.env.PORT || 3001
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
