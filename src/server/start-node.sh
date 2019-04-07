ec4de92550a836ca46097353bda49f5f3c66ba01
Kyle Cornwall <ckyle25@gmail.com>
2019-04-07 10:01:37 -0600

    edit start script

--- src/server/start-node.sh
+++ src/server/start-node.sh
@@ -3,6 +3,6 @@ echo "Starting Script"
 echo "Changing Directories"
 cd /home/cornwal2/public_html/
 echo "Starting server"
-npm run pm2start
+/home/cornwal2/bin/pm2 start index.js -i 1
 echo "Exiting"
 exit
